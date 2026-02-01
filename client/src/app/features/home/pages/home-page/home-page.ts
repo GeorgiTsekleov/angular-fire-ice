120813a0d2977a84599cff8f49ebda813440662f
Georgi Tsekleov <georgi.tsekleov@qaiware.com>
2026-02-01 22:51:03 +0200

    Refactor landing component location

--- client/src/app/features/home/pages/home-page/home-page.ts
+++ client/src/app/features/home/pages/home-page/home-page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { LandingCharacters } from '../../../../shared/components/landing-characters/landing-characters/landing-characters';
-import { LandingFeatures } from '../../../../shared/components/landing-features/landing-features/landing-features';
-import { LandingSaga } from '../../../../shared/components/landing-saga/landing-saga/landing-saga';
+import { LandingCharacters } from '../../../../shared/components/landing/landing-characters/landing-characters';
+import { LandingFeatures } from '../../../../shared/components/landing/landing-features/landing-features';
+import { LandingSaga } from '../../../../shared/components/landing/landing-saga/landing-saga';
 import { PrimaryButton } from '../../../../shared/components/buttons/primary-button/primary-button';
 import { SecondaryButton } from '../../../../shared/components/buttons/secondary-button/secondary-button';
 
