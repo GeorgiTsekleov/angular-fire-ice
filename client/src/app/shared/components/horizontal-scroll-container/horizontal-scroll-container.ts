c5ddb543e5bf20db5601332fe9f8390111f7968d
Georgi Tsekleov <georgi.tsekleov@qaiware.com>
2026-02-03 22:40:25 +0200

    Update horizontal scroll container by adding list wrapper inside

--- client/src/app/shared/components/horizontal-scroll-container/horizontal-scroll-container.ts
+++ client/src/app/shared/components/horizontal-scroll-container/horizontal-scroll-container.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-horizontal-scroll-container',
   standalone: true,
-  template: `<div class="horizontal-scroll"><ng-content></ng-content></div>`,
+  template: `<div class="horizontal-scroll">
+    <ul>
+      <ng-content></ng-content>
+    </ul>
+  </div>`,
   styleUrl: './horizontal-scroll-container.scss',
 })
 export class HorizontalScrollContainer {}
