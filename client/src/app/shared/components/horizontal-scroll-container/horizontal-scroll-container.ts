2c0f8fd1e057a52cdec95e28006dd18f9dfaf7f9
Georgi Tsekleov <georgi.tsekleov@qaiware.com>
2026-02-04 02:14:36 +0200

    Add justify-content options to horizontal scroll container component

--- client/src/app/shared/components/horizontal-scroll-container/horizontal-scroll-container.ts
+++ client/src/app/shared/components/horizontal-scroll-container/horizontal-scroll-container.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+type JustifyContent = 'start' | 'center' | 'end';
 
 @Component({
   selector: 'app-horizontal-scroll-container',
   standalone: true,
   template: `<div class="horizontal-scroll">
-    <ul>
+    <ul [class]="justifyContent">
       <ng-content></ng-content>
     </ul>
   </div>`,
   styleUrl: './horizontal-scroll-container.scss',
 })
-export class HorizontalScrollContainer {}
+export class HorizontalScrollContainer {
+  @Input() justifyContent: JustifyContent = 'start';
+}
