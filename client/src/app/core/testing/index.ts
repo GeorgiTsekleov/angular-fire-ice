cca3e8c5053f54b1c8369808577b7bd9fbdc98e8
Georgi Tsekleov <georgi.tsekleov@qaiware.com>
2026-02-04 02:45:23 +0200

    Add BookDetailFacade service

--- client/src/app/core/testing/index.ts
+++ client/src/app/core/testing/index.ts
@@ -1,5 +1,6 @@
 export * from './app-facade.mock';
 export * from './auth-facade.mock';
 export * from './books-facade.mock';
+export * from './book-detail-facade.mock';
 export * from './books-with-characters-facade.mock';
 export * from './favorites-facade.mock';
